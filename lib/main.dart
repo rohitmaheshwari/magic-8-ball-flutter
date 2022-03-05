e473d0166cb71de0722575424ef8a39d1e3715df
rohitmaheshwari <rohitkumar.balwani@gmail.com>
2022-03-06 00:06:09 +0500

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          backgroundColor: Colors.blue,
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  // const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset("./images/ball$ballNumber.png"),
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
